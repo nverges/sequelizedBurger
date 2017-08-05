89c4e8b5cccd9acb7310904a3a132aea4d86c2ec
nverges <nverges@email.arizona.edu>
2017-08-04 22:54:43 -0700

    still need delete route

--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -25,71 +25,26 @@ module.exports = function(app) {
         });
     });
 
-    // // Get route for retrieving a single post
-    // app.get("/burgers/:id", function(req, res) {
-    //     // Here we add an "include" property to our options in our findOne query
-    //     // We set the value to an array of the models we want to include in a left outer join
-    //     // In this case, just db.Author
-    //     db.Burger.findOne({
-    //         where: {
-    //             id: req.params.id
-    //         },
-    //         include: [db.Burger]
-    //     }).then(function(dbBurger) {
-    //         res.json(dbBurger);
-    //     });
-    // });
+    // GET api info. Displays all burgers in database in JSON format
+    app.get('/api', function(req, res) {
+        db.Burger.findAll({}).then(function(data) {
+            res.json(data);
+        });
+    });
 
-    // // POST route for saving a new post
-    // app.post("/burgers/create", function(req, res) {
-    //     db.Burger.create(req.body).then(function(dbBurger) {
-    //         res.redirect('/');
-    //     });
-    // });
-
-
-    // // PUT route for updating posts
-    // app.put("/api/Burger", function(req, res) {
-    //     db.Burger.update(
-    //         req.body, {
-    //             where: {
-    //                 id: req.body.id
-    //             }
-    //         }).then(function(dbBurger) {
-    //         res.json(dbBurger);
-    //     });
-    // });
-
-
-    app.post("/burgers/create", function(req, res) {
+    // POST route for creating a new burger
+    app.post("/api/burgers", function(req, res) {
 
         db.Burger.create({
             burger_name: req.body.burger_name,
             devoured: req.body.devoured
         }).then(function(data) {
-            res.redirect("/")
-        });
-    });
-
-    //updateOne
-    app.put("/burgers/:id", function(req, res) {
-        // var condition = "id = " + req.params.id;
-
-        db.Burger.update({
-            // burger_name: req.body.burger_name,
-            // devoured: req.body.devoured
-            devoured: true
-        }, {
-            where: {
-                id: req.body.burgerId
-            }
-        }).then(function(data) {
-            res.redirect("/")
+            res.redirect("/");
         });
     });
 
     // DELETE route for deleting posts
-    app.delete("/burgers/delete/:id", function(req, res) {
+    app.delete("/api/burgers/:id", function(req, res) {
         db.Burger.destroy({
             where: {
                 id: req.params.id
@@ -98,12 +53,5 @@ module.exports = function(app) {
             res.json(dbBurger);
         });
     });
-
-
-
-
-
-
-
 
 };
